1836909f2319e4992a9b888cf1f08e38b30c761d
yordankitov <kitov.yordan@gmail.com>
2022-03-01 15:17:48 +0200

    added s3 create bucket and upload file functions

--- src/upload_to_s3.py
+++ src/upload_to_s3.py
@@ -1,1 +1,17 @@
 import boto3
+
+client = boto3.resource('s3')
+
+
+def create_bucket(bucket_prefix):
+    bucket_name = bucket_prefix
+    bucket_response = client.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={
+            'LocationConstraint': 'eu-central-1'})
+
+    return bucket_response
+
+
+def upload_file(bucket_name, file_name):
+    client.Object(bucket_name, file_name).upload_file(Filename=file_name)
