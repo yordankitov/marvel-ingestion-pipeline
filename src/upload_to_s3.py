225b180ab95d94f8f243458628964794c9f41181
yordankitov <kitov.yordan@gmail.com>
2022-03-02 16:01:05 +0200

    added upload to s3 function; refactored other s3 functions

--- src/upload_to_s3.py
+++ src/upload_to_s3.py
@@ -3,9 +3,9 @@ import boto3
 client = boto3.client('s3')
 resource = boto3.resource('s3')
 
-def create_bucket(client, bucket_name):
+def create_bucket(bucket_name):
     try:
-        bucket_response = client.create_bucket(
+        bucket_response = resource.create_bucket(
             Bucket=bucket_name,
             CreateBucketConfiguration={
                 'LocationConstraint': 'eu-central-1'})
@@ -15,20 +15,32 @@ def create_bucket(client, bucket_name):
     return bucket_response
 
 
-def upload_file(client, bucket_name, file_name):
+def upload_file(bucket_name, file_name):
     try:
-        client.Object(bucket_name, file_name).upload_file(Filename=file_name)
+        resource.Object(bucket_name, file_name).upload_file(Filename=file_name)
     except Exception as e:
         print(e)
 
 
 def check_bucket_exists_and_you_have_permission_for_access():
-    response = client.head_bucket(
-        Bucket='il-tapde-final-exercise-yordan')
+    try:
+        response = client.head_bucket(
+            Bucket='il-tapde-final-exercise-yordan')
 
-    if response.get('ResponseMetadata').get('HTTPStatusCode') == 200:
-        print('mint')
-    else:
-        print('oops')
+        if response.get('ResponseMetadata').get('HTTPStatusCode') != 200:
+            pass
+        #     will have to do uploda the buckets
+        else:
+            print('You do not have permission to access the bucket or it does not exist!')
+    except Exception as e:
+        print(e)
 
 # check_bucket_exists_and_you_have_permission_for_access()
+
+def upload_file(bucket_name, file_name):
+    try:
+        resource.Object(bucket_name, file_name).upload_file(Filename=file_name)
+    except Exception as e:
+        print(e)
+
+# upload_file('il-tapde-final-exercise-yordan', 'data/characters_in_comics-final.csv')
