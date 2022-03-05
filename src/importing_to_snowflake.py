f8bc07f4634d2b6361936efc609eb756d03d0562
yordankitov <kitov.yordan@gmail.com>
2022-03-05 18:07:54 +0200

    added s3 stage creation and copy from stage to snowflake functions

--- src/importing_to_snowflake.py
+++ src/importing_to_snowflake.py
@@ -139,6 +139,54 @@ def database_config(con, schema):
     except Exception as e:
         print(e)
 
+def create_s3_stage_for_snowflake(con, db, schema):
+    create_storage_integration = """create storage integration IF NOT EXISTS s3_integration_yordan
+          type = external_stage
+          storage_provider = s3
+          enabled = true
+          storage_aws_role_arn = 'arn:aws:iam::185827676115:role/il-tapde-role-yordan'
+          storage_allowed_locations = ('s3://il-tapde-final-exercise-yordan/data/');"""
+
+    create_file_format = """create or replace file format {db}.{schema}.csv_format_yordan
+          type = csv
+          field_delimiter = ','
+          skip_header = 1
+          null_if = ('NULL', 'null')
+          empty_field_as_null = true
+          error_on_column_count_mismatch = false
+          field_optionally_enclosed_by = '"';""".format(db=db, schema=schema)
+
+    create_stage = """ 
+        create or replace stage {db}.{schema}.S3_STAGE
+        URL = 's3://il-tapde-final-exercise-yordan/data/'
+        storage_integration = s3_integration_yordan
+        file_format = {db}.{schema}.csv_format_yordan;""".format(db=db, schema=schema)
+
+
+    # separate func
+
+    try:
+        con.cursor().execute(create_storage_integration)
+        con.cursor().execute(create_file_format)
+        con.cursor().execute(create_stage)
+
+
+    except Exception as e:
+        print(e)
+
+
+def copy_s3_stage_to_sf(con, db, schema, entity):
+    copy_data = """ copy into {db}.{schema}.{entity}
+            from @{db}.{schema}.S3_STAGE
+            file_format= {db}.{schema}.csv_format_yordan;""".format(db=db, schema=schema, entity=entity)
+
+    try:
+        con.cursor().execute(copy_data)
+
+    except Exception as e:
+        print(e)
+
+
 def snowflake_connection(user, password, account, db, schema):
     """
     Establishes a connection to the db
@@ -160,6 +208,30 @@ def snowflake_connection(user, password, account, db, schema):
 
     return con
 
+
+def copy_to_snowflake(con, file_path, abbreviation, schema, table):
+    """
+    Establishes connection to Snowflake, creates the appropriate structure and dumps the data from a selected local file
+
+    :param con: The db it needs to be connected to
+    :param file_path: The path of the file used to import the data
+
+    """
+    try:
+        con.cursor().execute(
+            "PUT file://{file_path} @%{abbreviation}".format(file_path=file_path, abbreviation=abbreviation)
+        )
+        # con.cursor().execute("PUT file://../brewery_data/breweries.csv @%breweries")
+
+        con.cursor().execute(
+            "COPY INTO {db}.{schema}.{table} FROM @%{abbreviation} "
+            "file_format = (type = csv field_optionally_enclosed_by='\"' field_delimiter=',' SKIP_HEADER=1)".format(db=db, abbreviation=abbreviation, schema=schema, table=table)
+        )
+        con.cursor().execute("REMOVE @%{abbreviation}".format(abbreviation=abbreviation))
+    except Exception as e:
+        print(e)
+
+
 def populate_db():
     """
     Main function that structures everything to be executed chronologically
@@ -171,6 +243,12 @@ def populate_db():
 
     database_config(con, schema)
 
+    for entity in ['characters', 'creators', 'comics', 'events', 'characters_in_comics', 'characters_in_events', 'creators_in_comics']:
+        con.cursor().execute("truncate table {db}.{schema}.{entity}".format(db=db, schema=schema, entity=entity))
+        copy_to_snowflake(con, f'data/{entity}.csv', entity, schema, entity)
+
+    create_s3_stage_for_snowflake(con, db, schema)
+    copy_s3_stage_to_sf(con, db, schema, 'characters')
+
     con.close()
 
-populate_db()
