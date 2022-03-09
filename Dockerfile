d7a1e4ff9a5c316cd0af79adc05c12431f59595d
yordankitov <kitov.yordan@gmail.com>
2022-03-09 14:38:53 +0200

    added test for exception on request; added stages to gitlab ci

--- Dockerfile
+++ Dockerfile
@@ -9,11 +9,8 @@ RUN poetry config virtualenvs.create false
 RUN poetry install
 RUN poetry export -f requirements.txt --output requirements.txt
 
-RUN ls
-RUN pwd
-RUN find -name 'requirements.txt'
-#COPY build/requirements.txt requirements.txt
+
 RUN pip install -r requirements.txt
 COPY . .
 
-CMD ["python", "../src/main.py"]
+CMD ["python", "src/main.py"]
--- tests/unit/test_units.py
+++ tests/unit/test_units.py
@@ -7,6 +7,10 @@ from src.helpers import retries_session, generate_url
 from src.ingestion import ingest_entity
 
 
+
+url = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&limit=5&apikey=eef4a51f95df748e9c1effea6cc244f2&hash=fdc2fde7473f59a1544c0471493d6e1a&orderBy=modified&offset=0'
+
+
 def test_retries_session_returns_requests_session_object():
     session = retries_session()
     assert isinstance(session, requests.Session)
@@ -31,7 +35,7 @@ def test_fetching_raises_maximum_retries_exceeded_error(limit, offset, entity, o
 
 
 @pytest.mark.parametrize("limit, offset, entity, order_by, modified", [(1, 0, 'characters', 'modified', None)])
-def test_fetching_weather_forecast_with_successful_response(limit, offset, entity, order_by, modified):
+def test_fetching_characters_with_successful_response(limit, offset, entity, order_by, modified):
     expected_response = {"a": "dictionary"}
     mock_response = mock({"json": lambda: expected_response})
     with when(requests).get(...).thenReturn(mock_response):
@@ -42,7 +46,6 @@ def test_fetching_weather_forecast_with_successful_response(limit, offset, entit
 @responses.activate
 def test_chars_mock():
     # url = requests.get(generate_url('characters', 5), params={'orderBy': 'modified', 'offset': 0, 'modifiedSince': None}).url
-    url = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&limit=5&apikey=eef4a51f95df748e9c1effea6cc244f2&hash=fdc2fde7473f59a1544c0471493d6e1a&orderBy=modified&offset=0'
     response_test = {'data': {'count': 10, 'total': 20, 'offset': 0, 'results': [5]}}
     responses.add(responses.GET, url, json=response_test)
     expected_data = response_test['data']['results']
@@ -52,3 +55,10 @@ def test_chars_mock():
 
     assert expected_data == character_results['data']['results']
     assert expected_another_request == another_request
+
+@responses.activate
+def test_invalid_url_raises_exception():
+    responses.add(responses.GET, url,
+                  body=Exception('...'))
+    with pytest.raises(Exception):
+        requests.get('https://gateway.marvel.com:443/v1/c/characters?ts=1&limit=5&apikey=eef4a51f95df748e9c1effea6cc244f2&hash=fdc2fde7473f59a1544c0471493d6e1a&orderBy=modified&offset=0')
